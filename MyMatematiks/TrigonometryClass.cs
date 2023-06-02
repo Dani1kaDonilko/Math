dbf33a3719c57ac69a2321f17de262dfdbdac6b8
Danilka <daniil.donilko@gmail.com>
2023-06-02 17:47:01 +0300

    gdfg

---
+++ MyMatematiks/TrigonometryClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMatematiks
+{
+    public class TrigonometryClass
+    {
+        public static double Sin (double ac, double ab)
+        {
+            return ac / ab;
+        }
+        public static double Cos(double bc, double ab)
+        {
+            return bc / ab;
+        }
+        public static double Tan(double bc, double ac)
+        {
+            return bc / ac;
+        }
+        public static double CTan(double ac, double ab)
+        {
+            return ac / ab;
+        }
+        public static double ArcSin(double ac, double ab)
+        {
+            return ac / ab;
+        }
+
+    }
+}
